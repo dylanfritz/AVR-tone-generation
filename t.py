74244f19b459072a3f1e6cd981e5acc8a1de0d46
Dylan Fritz <100446167+dylanfritz@users.noreply.github.com>
2024-10-31 21:25:05 -0500

    Update t.py

--- t.py
+++ t.py
@@ -5,6 +5,10 @@ TONE_FREQ = 293.665
 def calculate_y(x):
     return (((((8*math.pow(10,6))/TONE_FREQ)-12)/x)-3)/3
 
+def calculate_x(y):
+    return (((8*math.pow(10,6))/TONE_FREQ)-12)/3*y+3
+
 
 
 print(calculate_y(87))
+print(calculate_x(52))
